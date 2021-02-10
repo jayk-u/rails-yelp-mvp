7788c8a0175663ca0f7824468caab0d2c1cfe9e3
jay cormier <jay.cormier@me.com>
2021-02-10 15:35:09 +0100

    completed Yelp test, could use a bit of front-polish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+fuji = { name: "Le Fuji", address: "Rue Derval, Rennes", phone_number: "02000000", category: "japanese"}
+kintaro = { name: "Kintaro", address: "Sainte-Anne, Paris", phone_number: "0100", category: "japanese"}
+ladess = { name: "Ladess", address: "Dans le 18ème, Paris", phone_number: "0102", category: "belgian"}
+cantine_voltaire = { name: "Cantine Voltaire", address: "11ème arrondissement, Paris", phone_number: "0103", category: "chinese"}
+chez_gladines = { name: "Chez Gladines", address: "quelque part dans le 13ème à Paris", phone_number: "02050201", category: "french"}
+
+[ fuji, kintaro, ladess, cantine_voltaire, chez_gladines ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
